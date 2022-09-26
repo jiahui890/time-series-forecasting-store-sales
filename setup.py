fac91887bbf0694507049e3b440acf5fde3abf3f
Jiahui <limjiahui1997@gmail.com>
2022-09-26 11:50:42 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Time Series Forecasting of Sales in Ecuador Stores',
+    author='jiahui',
+    license='MIT',
+)
